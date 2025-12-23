074dd00342927a06e27471d3456abd099dc8a987
Yelo-Pandora <yp_0721@outlook.com>
2025-12-23 09:38:27 +0800

    补回admin相关内容

--- python-backend/python-backend/management/context.py
+++ python-backend/python-backend/management/context.py
@@ -1,5 +1,5 @@
-from core.models import Eventarea, Otherarea, Event, Storearea, Facility
-
+from core.models import Eventarea, Otherarea, Event, Storearea, Facility, Admin
+from core.context import BaseContext
 
 class EventareaContext:
     """
@@ -362,3 +362,16 @@ class FacilityContext:
             facility_id: 设施ID
         """
         Facility.objects.filter(id=facility_id).delete()
+
+
+class AdminContext(BaseContext):
+    """针对 Admin 模型的基础 CRUD 和查询操作"""
+    def __init__(self):
+        super().__init__(Admin)
+
+    def get_by_account(self, account: str) -> Admin | None:
+        """根据账号名获取 Admin 实例"""
+        try:
+            return self.model.objects.get(account=account)
+        except self.model.DoesNotExist:
+            return None
